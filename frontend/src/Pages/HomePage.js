180e9b7a1f2dcc8863ac6e31c98b27b3ef3b6574
ckorir <chrisruto.k@gmail.com>
2024-02-17 00:41:00 +0300

    Added cards and destinations section

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -3,6 +3,8 @@ import Navbar from "../Layout/Navbar";
 import '../Css/Home.css';
 import Footer from "../Layout/Footer";
 import Carousel from "../Components/Carousel";
+import Cards from "../Components/Cards";
+import PhotoGrid from "../Components/PhotoGrid";
 
 
 export default function Home() {
@@ -10,6 +12,8 @@ export default function Home() {
         <div>
             <Navbar />
             <Carousel />
+            <Cards />
+            <PhotoGrid />
             <Footer/>
         </div>
     );
