ac7ff50515aa1382985be0217487aa881c9263a8
victor <victor.murithi@student.moringaschool.com>
2024-02-20 10:51:02 +0300

    added authentication in navbar

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,5 +1,4 @@
 import react from "react";
-import Navbar from "../Layout/Navbar";
 import '../Css/Home.css';
 import Footer from "../Layout/Footer";
 import Carousel from "../Components/Carousel";
@@ -10,7 +9,6 @@ import Services from "../Components/Services";
 export default function Home() {
     return (
         <div>
-            <Navbar />
             <Carousel />
             <Cards />
             <PhotoGrid />
