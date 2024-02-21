57492f516a44c6c7915548346e52ceed8d99491d
victor <victor.murithi@student.moringaschool.com>
2024-02-21 19:58:14 +0300

    updated chsnges

--- frontend/src/Layout/Navbar.js
+++ frontend/src/Layout/Navbar.js
@@ -10,7 +10,6 @@ export default function Navbar({ isAuthenticated }) {
         console.log("Logout clicked");
         localStorage.removeItem("token");
         window.location.reload();
-        navigate("/login");
     };
 
     return (
