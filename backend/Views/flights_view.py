7b67cae0c55d24073f1742b12e87d7c77637ae21
victor <victor.murithi@student.moringaschool.com>
2024-02-23 12:00:31 +0300

    fetching flights

--- backend/Views/flights_view.py
+++ backend/Views/flights_view.py
@@ -8,23 +8,16 @@ flights_bp = Blueprint("flights_bp", __name__)
 
 
 @flights_bp.route('/flights', methods=['POST'])
-def filter_flights_by_destination_and_flight_date():
+def filter_flights_by_destination():
     data = request.json
 
-    if not data or 'destination' not in data or 'flight_date' not in data:
-        return jsonify({"message": "Destination and flight date are required in the request body."}), 400
+    if not data or 'destination' not in data:
+        return jsonify({"message": "Destination is required in the request body."}), 400
 
     destination = data['destination']
-    flight_date = data['flight_date']
 
-    # Convert flight date string to a datetime object
-    try:
-        flight_date = datetime.strptime(flight_date, '%Y-%m-%d').date()
-    except ValueError:
-        return jsonify({"message": "Invalid flight date format. Use YYYY-MM-DD format."}), 400
-
-    # Filter flights based on the provided destination and flight date
-    flights = Flight.query.filter_by(destination=destination, departure_datetime=flight_date).all()
+    # Filter flights based on the provided destination
+    flights = Flight.query.filter_by(destination=destination).all()
 
     if flights:
         flights_data = [
@@ -32,7 +25,7 @@ def filter_flights_by_destination_and_flight_date():
                 "id": flight.id,
                 "airline": flight.airline,
                 "destination": flight.destination,
-                "departure_datetime": flight.departure_datetime,
+                "departure_datetime": flight.departure_datetime.strftime('%Y-%m-%d'),  # Format date as 'YYYY-MM-DD'
                 "price": flight.price,
                 "departure_airport_id": flight.departure_airport_id,
                 "arrival_airport_id": flight.arrival_airport_id
@@ -41,7 +34,7 @@ def filter_flights_by_destination_and_flight_date():
         ]
         return jsonify(flights_data), 200
     else:
-        return jsonify({"message": f"No flights found for destination '{destination}' on {flight_date}"}), 404
+        return jsonify({"message": f"No flights found for destination '{destination}'"}), 404
 
 @flights_bp.route('/<string:destination>/flights', methods=['GET'])
 def flights_to_destination(destination):
